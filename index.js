e2f490ea922c9b7e43d063e7563953606558cf93
afrye1997 <allisonfrye97@gmail.com>
2020-09-22 21:00:23 -0500

    idk

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express= require('express')
 const cors= require('cors')
 const app= express();
 const PORT=process.env.PORT || 3000;
-var connection = require("./Connection.js");
+var connection = require("./connection.js");
 app.use(cors());
 
 
