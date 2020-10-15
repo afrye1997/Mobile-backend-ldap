1a6e404d987e3e58ab9f93d507c171eb111e7fb7
afrye1997 <allisonfrye97@gmail.com>
2020-10-15 08:11:39 -0500

    first commit

--- index.js
+++ index.js
@@ -1,132 +1,179 @@
-"use strict"
-const express=require('express')
-const cors=require('cors')
-// const connection= require('./connection.js');
+#!/usr/bin/env nodejs
+"use strict";
+require("dotenv").config();
+const fetch = require("node-fetch");
+const express = require("express");
+const cors = require("cors");
+const axios = require('axios')
+const app = express();
+app.use(cors());
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
-var ldap = require('ldapjs');
+var ldap = require("ldapjs");
 var client = ldap.createClient({
-  url: 'ldaps://ds.uark.edu',
-  reconnect: true
+  url: "ldap://ds.uark.edu",
+  reconnect: false,
 });
 
-/*use this to create connection*/
-// function authenticateDN(username, password) {
-
-//     /*bind use for authentication*/
-//     client.bind("uid=ds-csce,ou=people,dc=uark,dc=edu", "B33z1t_p0w", function (err) {
-//         if (err) {
-//             console.log("Error in new connetion " + err)
-//         } else {
-//             /*if connection is success then go for any operation*/
-//             console.log("Success");
-//             // searchUser();
-//         }
-//     });
-// }
-
-// authenticateDN("uid=ds-csce,ou=people,dc=uark,dc=edu", "B33z1t_p0w")
-
-const app= express();
-const PORT=process.env.PORT || 3000;
-// const connection= require('./connection')
-
-app.use(cors());
-
+const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`)
-  })
-
-app.get('/', (req, res) => {
-  res.send('LDAP Test is running!!!!!')
-})
-
-app.get('/checking',  async (req, respond)=>{
-    // const connection= require('./connection')
-  //console.log("1", connection.connected)
-//   B33z1t_p0w
-  client.bind("uid=ds-csce,ou=people,dc=uark,dc=edu", "B33z1t_p0w!!!!!!!!!!!", function (err) {
-    if (err) {
-        console.log("Error in new connetion " + err)
-    } else {
-        /*if connection is success then go for any operation*/
-        console.log("Success!!!");
-        // searchUser();
-        const userUARK= req.query.userUARK; 
-        console.log(userUARK)
-        console.log(client.connected)
-    
-        var opts = {
-             filter:(`&(studentclasses=CSCE*)(uid=${userUARK})`),
-            //filter:(`&(studentclasses=CSCE*)(uid=af027)`),
-               scope: 'sub', 
-               attributes: ['uid','cn','mail','studentClasses','displayName']
-           };
-           
-           //base: which location i need to search
-           client.search('ou=people,dc=uark,dc=edu', opts, (err, res) => {
-          //  console.log("2", connection.connected)
-               if (err) {
-                   console.log("Error in search " + err)
-                   respond.send("eek")
-               } else {
-                   res.on('searchEntry', async (entry)=> {
-                   respond.send(entry.object)
-                   });
-                   res.on('searchReference', function (referral) {
-                       console.log('referral: ' + referral.uris.join());
-                   });
-       
-                   res.on('error', function (err) {
-                       console.error('error: ' + err.message);
-                       respond.send("eek")
-                   });
-                   res.on('end', function (result) {
-                       console.log(result.status)
-                   });
-                  
-               }
-           });
-    }
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
 
-    // const userUARK= req.query.userUARK; 
-    // console.log(userUARK)
-    // console.log(client.connected)
+app.get("/", (req, res) => {
+  res.send("allison");
+});
 
-    // var opts = {
-    //      filter:(`&(studentclasses=CSCE*)(uid=${userUARK})`),
-    //     //filter:(`&(studentclasses=CSCE*)(uid=af027)`),
-    //        scope: 'sub', 
-    //        attributes: ['uid','cn','mail','studentClasses','displayName']
-    //    };
-       
-    //    //base: which location i need to search
-    //    client.search('ou=people,dc=uark,dc=edu', opts, (err, res) => {
-    //   //  console.log("2", connection.connected)
-    //        if (err) {
-    //            console.log("Error in search " + err)
-    //            respond.send("eek")
-    //        } else {
-    //            res.on('searchEntry', async (entry)=> {
-    //            respond.send(entry.object)
-    //            });
-    //            res.on('searchReference', function (referral) {
-    //                console.log('referral: ' + referral.uris.join());
-    //            });
-   
-    //            res.on('error', function (err) {
-    //                console.error('error: ' + err.message);
-    //                respond.send("eek")
-    //            });
-    //            res.on('end', function (result) {
-    //                console.log(result.status)
-    //            });
+app.post("/login", (req, respond) => {
+  console.log(req.body);
+  const userUARK = req.body.user;
+  const userPW = req.body.password;
+  //
+  client.bind(
+    `uid= ${userUARK},ou=People,dc=uark,dc=edu`,
+    `${userPW}`,
+    function (err) {
+      if (err) {
+        respond.send("Incorrect login");
+      } else {
+        console.log("successful auth");
+
+        var opts = {
+          filter: `&(studentclasses=CSCE*)(uid=${userUARK})`,
+          scope: "sub",
+          attributes: ["uid", "givenName", "mail", "studentClasses", "displayName", "sn", "studentDepartments"],
+        };
+
+        //base: which location i need to search
+
+        client.search("ou=people,dc=uark,dc=edu", opts, (err, res) => {
+          if (err) {
+            console.log("Error in search " + err);
+            respond.send("User does not exist");
+          } else {
+            res.on("searchEntry", async (entry) => {
               
-    //        }
-    //    });
-    
-  })
+
+              const LDAPUSER = entry.object;
+
+              fetch(
+                `http://localhost:4000/users/getUser?USER_id=${userUARK}`
+              ).then((res) => {
+                if (res.status === 400) {
+                  //if person doesn't exist, let's add them
+                  const {
+                    uid,
+                    givenName,
+                    mail,
+                    studentClasses,
+                    displayName,
+                    sn,
+                    studentDepartments
+
+                  } = LDAPUSER;
+                  
+
+                  axios
+                    .post("http://localhost:4000/users/addUser", {
+                      
+                        USER_id: `${uid}`,
+                        USER_fName: `${givenName}`,
+                        USER_LName: `${sn}`,
+                        USER_email: `${mail}`
+                    })
+                    .then((res) => {
+                      console.log(`statusCode: ${res.statusCode}`);
+                      respond.send("worked"); //returns the ldap
+                    })
+                    .catch((error) => {
+                      console.error(error);
+                    });
+                }else{
+                  respond.send("was already in db")
+                }
+              });
+
+              //checking here
+              /**
+               * if( isAccountCreated)
+               *  return full object
+               * else if isaccount not created
+               *  call add new user
+               * return wrong credentials
+               */
+            });
+
+            res.on("searchReference", async (referral) => {
+              console.log("referral: " + referral.uris.join());
+            });
+
+            res.on("error", async (err) => {
+              console.error("error: " + err.message);
+              respond.send(err.message);
+            });
+
+            res.on("end", async (result) => {
+              console.log(result.status);
+            });
+          }
+        });
+      }
+    }
+  );
+});
+
+app.get("/search", async (req, respond) => {
+
+    client.bind(process.env.LDAP_USERNAME, process.env.LDAP_PASSWORD , function (
+    err
+  ) {
+    if (err) {
+      console.log("Error in new connetion " + err);
+    } else {
+      /*if connection is success then go for any operation*/
+      console.log("Successfully connected to LDAP");
+      // searchUser();
+      const userUARK = req.query.userUARK;
+      console.log(userUARK);
+      console.log(client.connected);
+
+      var opts = {
+        filter: `&(studentclasses=CSCE*)(uid=${userUARK})`,
+        scope: "sub",
+     //attributes: ["uid", "cn", "mail", "studentClasses", "displayName", "sn", "studentDepartments"],
+      };
+
+      //base: which location i need to search
+
+      client.search("ou=people,dc=uark,dc=edu", opts, (err, res) => {
+        if (err) {
+          console.log("Error in search " + err);
+          respond.send("User does not exist");
+        } else {
+
+          res.on("searchEntry", async (entry) => {
+            respond.send(entry.object);
+          });
+
+          res.on("searchReference", async (referral) => {
+            console.log("referral: " + referral.uris.join());
+          });
+
+          res.on("error", async (err) => {
+            console.error("error: " + err.message);
+            respond.send(err.message);
+          });
+
+          res.on("end", async (result) => {
+            console.log(result.status);
+          });
+        }
+      });
+    }
+  });
+});
 
 /**
  * 
